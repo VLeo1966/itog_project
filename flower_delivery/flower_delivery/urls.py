a133a47b3a5088ee0e13143ec5e7cc42b0c05300
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-11-01 03:13:45 +0300

    .

---
+++ flower_delivery/flower_delivery/urls.py
@@ -0,0 +1,9 @@
+# flower_delivery/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),  # Указываем users в качестве корневого маршрута
+]
+
---
+++ flower_delivery/users/models.py
@@ -0,0 +1,13 @@
+# users/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Можно добавить дополнительные поля, например:
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
