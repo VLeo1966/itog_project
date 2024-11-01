c48a1b4a52a8690213e1e431992d1370abf3be05
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-11-01 22:11:51 +0300

    /

--- flower_delivery/flower_delivery/urls.py
+++ flower_delivery/flower_delivery/urls.py
@@ -1,4 +1,6 @@
 # flower_delivery/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,5 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # Указываем users в качестве корневого маршрута
     path('', include('catalog.urls'))  # Каталог цветов
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
 
