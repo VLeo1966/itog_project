d13c9a5a8d6f83c9c7e34099176e9c5e88c5b0de
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-11-02 13:24:07 +0300

    .

--- flower_delivery/catalog/admin.py
+++ flower_delivery/catalog/admin.py
@@ -1,3 +1,6 @@
+# catalog/admin.py
 from django.contrib import admin
+from .models import Flower
 
-# Register your models here.
+admin.site.register(Flower)
+
--- flower_delivery/catalog/models.py
+++ flower_delivery/catalog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Flower(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='flowers/', blank=True, null=True)
 
     def __str__(self):
