e8e41a0dfb9df2a39f9280ea3e35ac2e0709ee9c
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-11-02 20:54:42 +0300

    .

--- flower_delivery/catalog/models.py
+++ flower_delivery/catalog/models.py
@@ -6,8 +6,11 @@ class Flower(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='flowers/', blank=True, null=True)
+    image = models.ImageField(upload_to='media/flowers/', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы'
