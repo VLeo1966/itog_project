2a93f841d90baa4f2f3bda4b86cdaf8ecb2f576e
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-11-05 17:55:13 +0300

    .

--- flower_delivery/catalog/models.py
+++ flower_delivery/catalog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Flower(models.Model):
+    objects = None
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
